8a542ddd52181b693b4453fc2ec2db92f2bc37c2
Ra7zZ <61758430+Ra7zZ@users.noreply.github.com>
2020-09-25 22:24:48 +0200

    update reverse.py

--- reverse.py
+++ reverse.py
@@ -1,8 +1,8 @@
 def funzione(lista):
-    for i in range(len(lista)):
-        if(type(lista[i])==list):
-            funzione(lista[i])
-            lista[i].reverse()
+    for var in lista:
+        if(type(var)==list):
+            funzione(var)
+            var.reverse()
     return
 
 array= [11, [7,8], 33, [1,2,3,4,5,6, [1,2,3, [55,99,0,1, [23,12]]],7,8,9,1000], 0]
