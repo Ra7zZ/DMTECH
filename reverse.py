840d817f368b9fbf28cc7f000862bda798fe239e
Ra7zZ <61758430+Ra7zZ@users.noreply.github.com>
2020-10-11 20:21:48 +0200

    Update reverse.py
    ora l'array originale rimane inalterato

--- reverse.py
+++ reverse.py
@@ -1,15 +1,20 @@
-def funzione(lista):
+from copy import deepcopy
+def recursive_reverse_list(lista):
     for var in lista:
         if(type(var)==list):
-            funzione(var)
+            recursive_reverse_list(var)
             var.reverse()
     return
 
 array= [11, [7,8], 33, [1,2,3,4,5,6, [1,2,3, [55,99,0,1, [23,12]]],7,8,9,1000], 0]
-print(array)
 
-array.reverse()
+print("Originale prima del reverse:\n"+str(array))
 
-funzione(array)
+arrayCopia=deepcopy(array)
 
-print(array)
+arrayCopia.reverse()
+
+recursive_reverse_list(arrayCopia)
+
+print("Originale dopo il reverse:\n"+str(array))
+print("Array dopo reverse:\n"+str(arrayCopia))
