b8d85b895ba728f93bf8f9738681613ca493a803
Ra7zZ <61758430+Ra7zZ@users.noreply.github.com>
2020-10-20 10:12:23 +0200

    Runs and plots
    parte e disegna la curva dei casi positivi quotidiani dal 24 febbraio2020

--- esercizi requests/esercizio4.py
+++ esercizi requests/esercizio4.py
@@ -1,6 +1,6 @@
 import requests
-import matplotlib
-import csv
+import matplotlib.pyplot as plt
+from dateutil import parser
 
 response = requests.get("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv")
 #print(response.text)
@@ -13,12 +13,25 @@ y = []
 
 
 with open("covid.csv", "r") as fp:
-    row_count = sum(1 for row in fp)
-    for i = 1 in fp.r:
+    #row_count = sum(1 for row in fp)
+    for line in fp:
         var = line.split(",")
-        if(line != "totale_positivi"):
+        if(var[6] != "totale_positivi"):
             y.append(float(var[6]))
-            x.append(var[1])
+        
+        if(var[0] != "data"):
+            #print(parser.parse(var[0]))
+            data = var[0].split("T")
+            giorno = data[0]
+            #print(giorno)
+            x.append(giorno)
 
-
+#datetime has no fromisoformat member
+#parser has no isoparse module
+#print(y)
 #print(x)
+
+plt.plot(x,y)
+plt.ylabel('totale_positivi')
+plt.xlabel('data')
+plt.show()
