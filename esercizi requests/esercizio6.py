d257f83b74673931b8b14d4bbdd2cd741bb2d11a
Ra7zZ <61758430+Ra7zZ@users.noreply.github.com>
2020-10-22 14:59:27 +0200

    Update esercizio6.py
    ora ritorna una lista di dizionari ma non fa il controllo sulle date multiple

--- esercizi requests/esercizio6.py
+++ esercizi requests/esercizio6.py
@@ -27,7 +27,6 @@ WHERE
 
 def get_results(endpoint_url, query):
     user_agent = "WDQS-example Python/%s.%s" % (sys.version_info[0], sys.version_info[1])
-    # TODO adjust user agent; see https://w.wiki/CX6
     sparql = SPARQLWrapper(endpoint_url, agent=user_agent)
     sparql.setQuery(query)
     sparql.setReturnFormat(JSON)
@@ -36,21 +35,42 @@ def get_results(endpoint_url, query):
 def get_film_of(anno):
 
     results = get_film(endpoint_url, anno)
-    film = {}
+    #film = {}
+    lista_film = []
     for result in results["results"]["bindings"]:
-        if "ImdbLabel" in result:
-            titolo = result["itemLabel"]["value"]
-            data = str(result["ddp"]["value"]).split("T")
-            pub_y = str(data[0])
-            imdb_cod = result["ImdbLabel"]["value"]
-            if imdb_cod not in film:
-                film[imdb_cod] = {"title" : titolo, "data" : pub_y}
-            else:
-                film[imdb_cod]["data"] = [film[imdb_cod].get("data")]
-                film[imdb_cod]["data"].append(pub_y)
-    print(film)
-    #return results
 
+        titolo = result["itemLabel"]["value"]
+        data = str(result["ddp"]["value"]).split("T")
+        pub_y = str(data[0])
+        
+        if "ImdbLabel" in result:
+            imdb_cod = result["ImdbLabel"]["value"]
+            lista_film.append(dict(codice=imdb_cod, title=titolo, data=pub_y))
+        else:
+            imdb_cod = "UNKNOWN"
+            lista_film.append(dict(codice=imdb_cod, title=titolo, data=pub_y))
+
+        """lista_film[imdb_cod] = [film[imdb_cod].get("data")]
+                film[imdb_cod]["data"].append(pub_y)"""
+
+    print(lista_film)
+    """print(lista_film)
+    return lista_film
+
+
+        
+        
+        #copia = film.copy()
+        #lista_film.append(copia)
+    #print(film)
+    #return lista_film"""
+
+"""'tt5952238': {'title': 'Una questione privata', 'data': '2020-09-03'}, 
+
+5952238 : {'imdb_cod' : 'tt5952238', 'title' : 'Una questione privata',  'data': '2020-09-03'}
+
+'tt2398149': {'title': "L'ufficiale e la spia", 'data': ['2020-02-06', '2020-04-02']}
+2398149 : {'imdb_cod' : 'tt2398149', 'title': "L'ufficiale e la spia", 'data': ['2020-02-06', '2020-04-02']}"""
 
 """'item': {'type': 'uri', 'value': 'http://www.wikidata.org/entity/Q305839'},
 'ddp': {'datatype': 'http://www.w3.org/2001/XMLSchema#dateTime', 'type': 'literal', 'value': '1940-01-01T00:00:00Z'},
